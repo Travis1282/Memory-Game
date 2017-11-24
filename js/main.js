99cf57192da56b3b0bae0d8a9c0fd63bbafba6b8
Travis1282 <tmail20@gmail.com>
2017-11-23 21:55:39 -0600

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,28 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "king";
-var cardFour = "queen";
-console.log("User flipped " + cardOne);
+
+
+var cards=["queen", "queen", "king", "king"];
+var cardsInPlay=[];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+/*var cardThree = cards[2];
+cardsInPlay.push(cardThree);
+var cardFour = cards[3];
+cardsInPlay.push(cardFour);
+*/
+
+//console.log("User flipped " + cardThree);
+//console.log("User flipped " + cardFour);
+console.log(cardsInPlay);
+console.log(cardsInPlay.length);
+if(cardsInPlay.length === 2){
+console.log(cardOne, cardTwo);
+if (cardOne === cardTwo) {
+	alert("You found a match!");
+}else{
+	alert("You found a match!");}
+};
+
+
